f0837dca98af7628801e100cda168d1b2161a2cb
yuliia.valentine <Yuliia.valentine@gmail.com>
2022-07-07 12:02:15 +0300

    instal primeng

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/movie';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-movies',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies.component.scss']
 })
 export class MoviesComponent implements OnInit {
+  movies: Movie[] = [];
 
-  constructor() { }
+  constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
+    this.moviesService.searchMovies(1).subscribe((movies) => {
+      this.movies = movies;
+    });
   }
-
 }
